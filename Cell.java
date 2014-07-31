a2fa799b3e2112489d954984c97800743d70850d
Nevyn2345 <sbarnett1@sheffield.ac.uk>
2014-07-31 14:34:08 +0100

    test

--- Cell.java
+++ Cell.java
@@ -1,4 +1,5 @@
 class Cell {
+	//instantiates an Cell object
         int blue_abs = 0;
         int green_abs = 1;
         int red_abs = 0;
@@ -18,29 +19,29 @@ class Cell {
 
         int updateEnergy( int energy, int blue, int green, int red) {
             energy = energy + blue + green + red;
-            return energy;
+            return energy;//tracks a cells energy
         }
 
         int updateAbsorbtion( int colour, int increment ) {
             colour = colour + increment;
-            return colour;
+            return colour; //tracks the amount a cell can absorb
         }
 
         int endofTurn ( int energy, int time_count ) {
             if (time_count % 5 == 4 ) {
                 energy = energy/2 ;
             }
-            return energy;
+            return energy; //tracks the end of turn for the game,
         }
 
         int[] buyCarbon( int energy, int carbon_current, int carbon_desired, int carbon_price ) {
             energy = energy - ( carbon_desired * carbon_price );
             carbon_current = carbon_current + carbon_desired;
-            return new int[] { energy, carbon_current };
+            return new int[] { energy, carbon_current }; //buy carbon, lower energy
         }
 
         int[] buyResource( int energy, int resource_current, int resource_desired, int resource_price ) {
             energy = energy - ( resource_desired * resource_price );
-            resource_current = resource_current + resource_desired;
+            resource_current = resource_current + resource_desired; //buy resource
         }
 }
