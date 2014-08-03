54a386a3df7ed0aa96622d375dbdfc17ce9b3e96
Nevyn2345 <sbarnett1@sheffield.ac.uk>
2014-08-03 11:11:08 +0100

    4-S- Working user input and buying genes

--- CellMain.java
+++ CellMain.java
@@ -9,26 +9,30 @@ class CellMain {
         System.out.println( "The cell's new position is: " + cell_1.position[0]
             + "," + cell_1.position[1] + "," + cell_1.position[2] + "...");
 
+        Scanner sc = new Scanner(System.in);
+
         for (int i = 0; i < 3; i++) {
             cell_1.time_count++; 
             cell_1.blue_abs = cell_1.updateAbsorbtion( cell_1.blue_abs, 1 );
             cell_1.energy = cell_1.updateEnergy( cell_1.energy, cell_1.blue_abs, cell_1.green_abs, cell_1.red_abs );
             System.out.println("Do you want to buy a gene?");
-            String response = Input.readline();
-            
-            if (response == "yes"){
+            String response = sc.nextLine();
+            if (response.equals("yes")){ 
                 for (int j = 0; j < cell_1.possible_genes.size(); j++){
                     System.out.println(cell_1.possible_genes.get(j));
                 }
-                String gene = Input.readline();
+                String gene = sc.nextLine();
                 List<ArrayList<String>> unpack = cell_1.buy_gene( gene, cell_1.possible_genes, cell_1.current_genes);
                 cell_1.possible_genes = unpack.get(0);
                 cell_1.current_genes = unpack.get(1);
+                System.out.println(cell_1.current_genes);
             }
+            System.out.println("What direction do you want to move? [0-5]");
+            int movement = sc.nextInt();
+            cell_1.position = cell_1.changePosition( cell_1.position, movement );
+            System.out.println("Your new position is: " + cell_1.position);
 
-
-
-	         if (cell_1.energy > 6) {
+	        if (cell_1.energy > 6) {
                 int[] temp = cell_1.buyCarbon( cell_1.energy, cell_1.carbon, 1, cell_1.carbon_price);
                 System.out.println( "Bought 1 carbon for a total of:" + cell_1.carbon );
                 cell_1.energy = temp[0];
